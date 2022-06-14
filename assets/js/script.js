133219d8925cae2a0d35dd5c5701e7dcd3cca8ad
Kelly Wacker <kelwacker@gmail.com>
2022-06-14 16:53:54 -0400

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $("#currentDay").replaceWith(currentTime);
 $("textarea").on("click", function() {
     var text = $(this).text().trim();
     var textInput = $("<textarea>")
-    .val(text);
+        .val(text);
 
   textInput.trigger("focus");
 });
@@ -18,10 +18,8 @@ var checkDates = function() {
 
     // a loop for each time block
     $(".time-block").each(function () {
-        // splits the id into two parts to give a value
+        // splits the id attr. into two parts to give a value to check
         var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-        // a check
-        console.log( timeBlock, currentHour)
         // removes classes for automatic checks
         $(this).removeClass("past present future")
         // changes the color based on the value of the timeBlock
@@ -36,6 +34,48 @@ var checkDates = function() {
         }
     })
 };
+
+// click button function
+$(".saveBtn").on("click", function() {
+    //setting the local storage for each hour
+    var am9 = document.getElementById("9").value;
+    localStorage.setItem("9am Event", am9);
+
+    var am10 = document.getElementById("10").value;
+    localStorage.setItem("10am Event", am10);
+
+    var am11 = document.getElementById("11").value;
+    localStorage.setItem("11am Event", am11);
+
+    var pm12 = document.getElementById("12").value;
+    localStorage.setItem("12pm Event", pm12);
+
+    var pm1 = document.getElementById("1").value;
+    localStorage.setItem("1pm Event", pm1);
+
+    var pm2 = document.getElementById("2").value;
+    localStorage.setItem("2pm Event", pm2);
+
+    var pm3 = document.getElementById("3").value;
+    localStorage.setItem("3pm Event", pm3);
+
+    var pm4 = document.getElementById("4").value;
+    localStorage.setItem("4pm Event", pm4);
+
+    var pm5 = document.getElementById("5").value;
+    localStorage.setItem("5pm Event", pm5);
+});
+
+// display the local storage 
+$("#9").val(localStorage.getItem("9am Event"));
+$("#10").val(localStorage.getItem("10am Event"));
+$("#11").val(localStorage.getItem("11am Event"));
+$("#12").val(localStorage.getItem("12pm Event"));
+$("#13").val(localStorage.getItem("1pm Event"));
+$("#14").val(localStorage.getItem("2pm Event"));
+$("#15").val(localStorage.getItem("3pm Event"));
+$("#16").val(localStorage.getItem("4pm Event"));
+$("#17").val(localStorage.getItem("5pm Event"));
 
 // calls the function to check hour and color correctly
 checkDates();
